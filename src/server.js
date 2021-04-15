2c1f8bf1e3eb18bf303cba97e5945f1dcfee8081
dillonhammer <dillonhammer@gmail.com>
2021-04-15 01:30:11 -0400

    No duplicate names, end_clap

--- src/server.js
+++ src/server.js
@@ -22,9 +22,21 @@ const io = socketIo(server, {
   },
 });
 
+/*
+Example State:
+{
+  names: Set<string>,
+  socketToName: Map<string, string>,
+  count: number,
+  clapping: string[],
+}
+*/
+
 const state = {
+  names: new Set(),
+  socketToName: new Map(),
   count: 0,
-  clapping: 0,
+  clapping: [],
 };
 
 io.on("connect", (socket) => {
@@ -33,22 +45,41 @@ io.on("connect", (socket) => {
 
   socket.on("disconnect", () => {
     console.log("Disconnected client", state);
+    const name = state.socketToName.get(socket.id);
+    state.names.delete(name);
+    state.socketToName.delete(socket.id);
     state.count--;
+    state.clapping.splice(name);
     io.sockets.emit("update", state);
   });
 
   socket.on("enter", (name) => {
-    console.log(`${name} joined!`);
+    if (state.names.has(name)) {
+      socket.emit("error", "Name already taken! :(");
+    } else {
+      state.names.add(name);
+      state.socketToName.set(socket.id, name);
+      console.log(`${name} joined!`);
+      io.sockets.emit("update", state);
+    }
+  });
+
+  socket.on("clap", (name) => {
+    state.clapping.push(name);
     io.sockets.emit("update", state);
   });
 
-  socket.on("clap", () => {
-    state.clapping++;
+  socket.on("end_clap", (name) => {
+    state.clapping.splice(state.clapping.indexOf(name));
+    console.log("end_clap", state);
     io.sockets.emit("update", state);
   });
 
-  socket.on("end_clap", () => {
-    state.clapping--;
+  socket.on("clear", () => {
+    state.names = new Set();
+    state.socketToName = new Map();
+    state.count = 1;
+    state.clapping = [];
     io.sockets.emit("update", state);
   });
 });
